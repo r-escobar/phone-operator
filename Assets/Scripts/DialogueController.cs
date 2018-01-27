bda0ac6255b0ae8b9f63bd0eaecde8918a7be09b
Paulo Alves <p@uloalv.es>
2018-01-27 17:30:21 -0600

    Add Dialogue Controller

---
+++ Assets/Scripts/DialogueController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+
+[System.Serializable]
+public class SpeakerPair {
+	public Speaker speaker1;
+	public Speaker speaker2;
+}
+
+[System.Serializable]
+public class Speaker {
+	public string name;
+	public Color color;
+	public string[] rambling;
+	public string matchTest;
+	public string correctMatchResponse;
+	public string wrongMatchResponse;
+}
+
+public class DialogueController : MonoBehaviour {
+
+	public SpeakerPair test;
+
+	void Start()
+	{
+		test = LoadSpeakerPair();
+	}
+
+	public SpeakerPair LoadSpeakerPair()
+	{
+		string file = "Convo-Billy";
+		TextAsset asset = Resources.Load (Path.Combine ("Dialogue", file)) as TextAsset;
+		SpeakerPair sp = JsonUtility.FromJson<SpeakerPair> (asset.text);
+		return sp;
+	}
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+A game about operating an old-school telephone switchboard to connect pairs of people who are trying to talk.
+
+Made at Global Game Jam 2018.
