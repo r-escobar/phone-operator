d25460d864d6b5147a447903b6d9b48d084ff57a
Ricardo Escobar <rescobar713@gmail.com>
2018-01-27 23:36:44 -0600

    Working with textu stuff in TestScene

--- Assets/TextMesh Pro/Examples & Extras/Scripts/TeleType.cs
+++ Assets/TextMesh Pro/Examples & Extras/Scripts/TeleType.cs
@@ -23,9 +23,9 @@ namespace TMPro.Examples
         {
             // Get Reference to TextMeshPro Component
             m_textMeshPro = GetComponent<TMP_Text>();
-            m_textMeshPro.text = label01;
-            m_textMeshPro.enableWordWrapping = true;
-            m_textMeshPro.alignment = TextAlignmentOptions.Top;
+            //m_textMeshPro.text = label01;
+            //m_textMeshPro.enableWordWrapping = true;
+            //m_textMeshPro.alignment = TextAlignmentOptions.Top;
 
 
 
@@ -55,21 +55,21 @@ namespace TMPro.Examples
             int counter = 0;
             int visibleCount = 0;
 
-            while (true)
+            while (visibleCount < totalVisibleCharacters)
             {
                 visibleCount = counter % (totalVisibleCharacters + 1);
 
                 m_textMeshPro.maxVisibleCharacters = visibleCount; // How many characters should TextMeshPro display?
 
                 // Once the last character has been revealed, wait 1.0 second and start over.
-                if (visibleCount >= totalVisibleCharacters)
-                {
-                    yield return new WaitForSeconds(1.0f);
-                    m_textMeshPro.text = label02;
-                    yield return new WaitForSeconds(1.0f);
-                    m_textMeshPro.text = label01;
-                    yield return new WaitForSeconds(1.0f);
-                }
+                //if (visibleCount >= totalVisibleCharacters)
+                //{
+                //    yield return new WaitForSeconds(1.0f);
+                //    m_textMeshPro.text = label02;
+                //    yield return new WaitForSeconds(1.0f);
+                //    m_textMeshPro.text = label01;
+                //    yield return new WaitForSeconds(1.0f);
+                //}
 
                 counter += 1;
 
