63f94f4a7eac61b23f90af82a6f7f94ab602b57f
Ricardo Escobar <rescobar713@gmail.com>
2018-01-27 21:11:02 -0600

    Light pulsating

--- Assets/Scripts/LightController.cs
+++ Assets/Scripts/LightController.cs
@@ -4,15 +4,22 @@ using UnityEngine;
 
 public class LightController : MonoBehaviour {
 
-    public Color litColor;
+    Color litColor;
 
     public float pulseDuration = 1f;
     public float pulseTiming = 0.5f;
 
+    public SpriteRenderer sRend;
+
+    
+    public bool pulsateOnStart = false;
+
+    public bool pulsating = false;
 
 	// Use this for initialization
 	void Start () {
-		
+        if(pulsateOnStart)
+            StartPulsating(Color.green);
 	}
 	
 	// Update is called once per frame
@@ -20,13 +27,46 @@ public class LightController : MonoBehaviour {
 		
 	}
 
-    public void StartPulsating()
+    public void StartPulsating(Color litCol)
     {
+        litColor = litCol;
+        pulsating = true;
+        PulseToColor();
+    }
 
+    public void PulseToColor()
+    {
+        if (!pulsating)
+            return;
+        iTween.ValueTo(gameObject, iTween.Hash(
+            "from", Color.white,
+            "to", litColor,
+            "time", pulseDuration / 2f,
+            "easetype", "easeInQuad",
+            "onUpdate", "UpdateColor",
+            "onComplete", "PulseFromColor"
+            ));
+    }
+
+    public void PulseFromColor()
+    {
+        iTween.ValueTo(gameObject, iTween.Hash(
+            "from", litColor,
+            "to", Color.white,
+            "time", pulseDuration / 2f,
+            "easetype", "easeOutQuad",
+            "onUpdate", "UpdateColor",
+            "onComplete", "PulseToColor"
+            ));
     }
 
     public void StopPulsating()
     {
+        pulsating = false;
+    }
 
+    public void UpdateColor(Color newCol)
+    {
+        sRend.color = newCol;
     }
 }
