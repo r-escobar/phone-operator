148c5412aa07466aefb6b2f150b3adf94b52bccc
Paulo Alves <p@uloalv.es>
2018-01-28 14:16:59 -0600

    Next Speaker comes from correct source

--- Assets/Scripts/DialogueController.cs
+++ Assets/Scripts/DialogueController.cs
@@ -6,15 +6,17 @@ using UnityEngine;
 public class DialogueController : MonoBehaviour {
 
 	public List<SpeakerPair> speakerPairs;
+	public List<Speaker> speakersWhosePairsAreOnBoard;
+
 	public List<string> speakersPerWave;
     // 'N' corresponds with getting a speaker from a NEW pair (i.e. it's partner hasn't been put on the board yet)
     // 'U' corresponds with getting a speaker from a USED par (i.e. it's partner is on the board)
 
-    public JackController jackToSpeak;
+	public int curWave;
 
 	void Start()
 	{
-        jackToSpeak.Speak("test string!", 5f);
+		curWave = 0;
 
 		for(int i = 1; i <= 30; i++) {
 			speakerPairs.Insert(i-1, LoadSpeakerPair(1));
@@ -31,6 +33,39 @@ public class DialogueController : MonoBehaviour {
 	}
 
 	public void NextWave() {
+		string curWaveSpeakers = speakersPerWave[curWave];
+		for(int i = 0; i < curWaveSpeakers.Length; i++) {
+			switch (curWaveSpeakers[i])
+			{
+				case 'N':
+					AddSpeakerOfNewPairToBoard();
+					break;
+				case 'U':
+					AddSpeakerWhosePairIsOnBoardToBoard();
+					break;
+				default:
+					print("Next speaker source is neither acceptable option.");
+					break;
+			}
+		}
+		curWave++;
+	}
+
+	void AddSpeakerOfNewPairToBoard() {
+		SpeakerPair pairToLoad = speakerPairs[0];
+		speakerPairs.RemoveAt(0);
+		// Load pairToLoad.speaker1
+		speakersWhosePairsAreOnBoard.Add(pairToLoad.speaker2);
+	}
+
+	void AddSpeakerWhosePairIsOnBoardToBoard() {
+
+		int randomPartnerIndex = Random.Range(0,speakersWhosePairsAreOnBoard.Count);
+		Speaker partnerToLoad = speakersWhosePairsAreOnBoard[randomPartnerIndex];
+		speakersWhosePairsAreOnBoard.RemoveAt(randomPartnerIndex);
+
+		// Load partnerToLoad
 
 	}
+
 }
