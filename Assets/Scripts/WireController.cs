6858edc8d3be1fbca44dd7d4254e9ba5e83f6919
Ricardo Escobar <rescobar713@gmail.com>
2018-01-27 19:45:02 -0600

    Switchboard layout changes

--- Assets/Scripts/WireController.cs
+++ Assets/Scripts/WireController.cs
@@ -7,9 +7,13 @@ public class WireController : MonoBehaviour {
     public GameObject plug1;
     public GameObject plug2;
 
-    public GameObject wireTap;
+
+    public GameObject wireJoint1;
+    public GameObject wireJoint2;
 
     LineRenderer lineRend;
+
+    public float wireJointAngle;
 
 	// Use this for initialization
 	void Start () {
@@ -21,9 +25,15 @@ public class WireController : MonoBehaviour {
         lineRend.SetPosition(0, plug1.transform.position);
         lineRend.SetPosition(1, plug2.transform.position);
 
-        wireTap.transform.position = (plug1.transform.position + plug2.transform.position) / 2f;
+        //wireTap.transform.position = (plug1.transform.position + plug2.transform.position) / 2f;
         
-        //float wireTapAngle = Mathf.Atan((plug2.transform.position.y - plug1.transform.position.y) / (plug2.transform.position.x - plug1.transform.position.x));
-        //wireTap.transform.eulerAngles = new Vector3(0f, 0f, Mathf.Rad2Deg * wireTapAngle);
+        wireJointAngle = Mathf.Atan((plug2.transform.position.y - plug1.transform.position.y) / (plug2.transform.position.x - plug1.transform.position.x));
+
+        float angleMod = (plug2.transform.position.x - plug1.transform.position.x < 0) ? 180f : 0f;
+
+        wireJoint1.transform.eulerAngles = new Vector3(0f, 0f, (Mathf.Rad2Deg * wireJointAngle) + (180f - angleMod));
+
+
+        wireJoint2.transform.eulerAngles = new Vector3(0f, 0f, (Mathf.Rad2Deg * wireJointAngle) + angleMod);
     }
 }
